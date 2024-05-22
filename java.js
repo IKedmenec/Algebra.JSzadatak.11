18b6a0b2601e01d062d2123663acce002d813594
IKedmenec <ivan.kec86@gmail.com>
2024-05-22 19:19:15 +0200

    promjena1

---
+++ java.js
@@ -0,0 +1,17 @@
+
+function zbroji() {
+var n = 3;
+let m = 4;
+
+console.log(n + m);
+
+}
+
+zbroji();
+
+document.getElementById('box1').inputMode
+
+
+
+
+document.getElementById('box4').setAttribute('readonly', true);
